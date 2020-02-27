596f1914a9376707b3f001be2c245a7306e5cc09
Saleh Enam Shohag <shohag402@gmail.com>
2020-02-27 22:08:52 +0600

    [Titile] Jazzy Documentation project added
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ Documentation/Documentation/HealthDataStore.swift
@@ -0,0 +1,30 @@
+//
+//  HealthDataStore.swift
+//  Documentation
+//
+//  Created by Saleh Enam Shohag on 27/2/20.
+//  Copyright © 2020 Saleh Enam Shohag. All rights reserved.
+//
+
+import Foundation
+
+///This class can be used to connect
+public class HealthDataStore{
+    
+    /**
+    Call this function for showing alert with OK and Cancel button in your View Controller class.
+    - Parameters:
+       - VC : View Controller over which the function is called. You can use self, or provide view controller name.
+       - message: Pass your alert message in String.
+       - okClickHandler: This will give you call back inside block when OK button is clicked
+    
+    ### Usage Example: ###
+    ````
+    AlertClass().showAlert(self, andMessage: "This is custom alert") { (okClick) in
+    }
+    ````
+    */
+    public func publicFunction(){
+        
+    }
+}
---
+++ Documentation/Documentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documentation
+//
+//  Created by Saleh Enam Shohag on 27/2/20.
+//  Copyright © 2020 Saleh Enam Shohag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
